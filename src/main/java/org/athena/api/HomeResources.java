2ff0fc0d5151bd0240f1770576c1cb02f6f34c59
panh1992 <panh1992@163.com>
2018-11-19 17:00:32 +0800

    添加 RowMapper 测试

--- src/main/java/org/athena/api/HomeResources.java
+++ src/main/java/org/athena/api/HomeResources.java
@@ -1,6 +1,7 @@
 package org.athena.api;
 
 import com.google.common.collect.Maps;
+import org.athena.entity.User;
 import org.athena.jdbi.UserRepository;
 
 import javax.ws.rs.GET;
@@ -8,7 +9,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import java.io.IOException;
+import java.util.List;
 import java.util.Map;
+import java.util.UUID;
 
 @Path("/")
 @Produces(MediaType.APPLICATION_JSON)
@@ -26,6 +29,12 @@ public class HomeResources {
         map.put("修改前", "sdfghjk");
         userRepository.testUser();
         throw new IOException("asdfg");
+    }
+
+    @GET
+    @Path("/list")
+    public List<User> findAll() {
+        return userRepository.findAll();
     }
 
 }
--- src/main/java/org/athena/jdbi/UserRepository.java
+++ src/main/java/org/athena/jdbi/UserRepository.java
@@ -1,6 +1,9 @@
 package org.athena.jdbi;
 
+import org.athena.entity.User;
+import org.athena.entity.mapper.UserMapper;
 import org.jdbi.v3.core.transaction.TransactionIsolationLevel;
+import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
 import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 import org.jdbi.v3.sqlobject.transaction.Transaction;
@@ -10,11 +13,15 @@ import java.util.List;
 
 public interface UserRepository {
 
-    @SqlQuery("select username from users")
+    @SqlQuery("SELECT username FROM users")
     List<String> findName();
 
-    @SqlUpdate("update users set username = 'asdfg'")
+    @SqlUpdate("UPDATE users SET username = 'asdfg'")
     void updateUser();
+
+    @SqlQuery("SELECT id, username, password FROM users")
+    @RegisterRowMapper(UserMapper.class)
+    List<User> findAll();
 
     @Transaction(TransactionIsolationLevel.READ_COMMITTED)
     default void testUser() throws IOException {
