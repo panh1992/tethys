9562d482ce2485404b53cf11871436a9f20bafc0
panh1992 <panh1992@163.com>
2019-03-07 15:49:08 +0800

    commit

--- src/main/java/org/athena/util/Constant.java
+++ src/main/java/org/athena/util/Constant.java
@@ -13,8 +13,8 @@ public final class Constant {
 
     public static final Integer AUTHORIZATION_DURATION = 120;
 
-    public static final String TIME_ZONE = "UTC";
+    static final String TIME_ZONE = "UTC";
 
-    public static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
+    static final String DATE_TIME_FORMAT = "yyyy-MM-dd HH:mm:ss";
 
 }
